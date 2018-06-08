c7ee0b66adff4bbe084e6d3c275a231d2fa09ebe
Shubham Bauskar <shubhambauskar80@gmail.com>
2018-06-08 09:18:48 +0000

    Add seccure password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180608075256) do
+ActiveRecord::Schema.define(version: 20180608091223) do
 
   create_table "contests", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180608075256) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
